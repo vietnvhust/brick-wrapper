723debd64ec8f72ebc671204310e3b37b5904b60
Nguyen Khong <vietnv.hust@gmail.com>
2021-01-21 16:07:23 +0700

    add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/ui/header/Header";
+import { GlobalStyle } from './assets/containts/globalStyled';
 function App() {
     return (
         <Router>
+            <GlobalStyle/>
             <Header />
         </Router>
     );
