e91c2f195933b8f1589b09fe01bc2f82810488bf
Nguyen Khong <vietnv.hust@gmail.com>
2021-01-22 10:18:24 +0700

    install react typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/ui/header/Header";
-import { GlobalStyle } from './assets/containts/globalStyled';
-import MainRouter from "./routers/Main.router";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
-    return (
-        <Router>
-            <GlobalStyle/>
-            <Header />
-            <MainRouter/>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
