c5406b01cc1eccbb06984461752368dc6d46be66
Nguyen Khong <vietnv.hust@gmail.com>
2021-01-22 09:10:57 +0700

    add router success

--- src/routers/Main.router.tsx
+++ src/routers/Main.router.tsx
@@ -1,20 +1,13 @@
 import React, { Suspense } from 'react';
 import Loading from "../components/core/Loading";
 import { Switch, Route } from 'react-router-dom';
-// const HomePage = React.lazy(() => import('container/homePage/HomePage'));
-// const DashBoard = React.lazy(() => import('container/dashboard/DashBoard'));
-// const Wrapper = React.lazy(() => import('container/wrapper/Wrapper'));
-// const UnWrapper = React.lazy(() => import('container/unWrapper/UnWrapper'));
-// const DashBoardUser = React.lazy(() => import('container/dashboardUser/DashBoardUser'));
-// const Page404 = React.lazy(() => import('container/page404/Page404'));
-// const SearchUser = React.lazy(() => import('container/searchPage/SearchUser'));
-import HomePage from "../container/homePage/HomePage";
-import DashBoard from "../container/dashboard/DashBoard";
-import Wrapper from "../container/wrapper/Wrapper";
-import UnWrapper from "../container/unWrapper/UnWrapper";
-import DashBoardUser from "../container/dashboardUser/DashBoardUser";
-import SearchUser from "../container/searchPage/SearchUser";
-import Page404 from "../container/page404/Page404";
+const HomePage = React.lazy(() => import('container/homePage/HomePage'));
+const DashBoard = React.lazy(() => import('container/dashboard/DashBoard'));
+const Wrapper = React.lazy(() => import('container/wrapper/Wrapper'));
+const UnWrapper = React.lazy(() => import('container/unWrapper/UnWrapper'));
+const DashBoardUser = React.lazy(() => import('container/dashboardUser/DashBoardUser'));
+const Page404 = React.lazy(() => import('container/page404/Page404'));
+const SearchUser = React.lazy(() => import('container/searchPage/SearchUser'));
 const routers = [
     {
         path: '/',
@@ -58,15 +51,15 @@ interface MainRouterProps {
 }
 function MainRouter() {
     return (
-        // <Suspense fallback={<Loading/>}>
+        <Suspense fallback={<Loading/>}>
             <Switch>
                 {
-                    routers.map(({ path, component }:MainRouterProps,index) => {
-                       <Route key={index} path={path} component={component} />
-                    })
+                    routers.map(({ component, path, ...rest }: MainRouterProps, index: number) =>
+                        <Route key={index} component={component} path={path} {...rest} />
+                    )
                 }
             </Switch>
-        // </Suspense>
+        </Suspense>
     );
 }
 
